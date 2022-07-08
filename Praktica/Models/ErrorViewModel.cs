3fe8bef929bc54e6e236e85c0de4e474c929547e
szubakov <90329069+szubakov@users.noreply.github.com>
2022-07-08 14:32:03 +0500

    Версия 1.08.07.2022
    Создали репозиторий

---
+++ Praktica/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Praktica.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
